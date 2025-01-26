a07a09be395217e2decdb1ae938b379b085980d0
Ruhul Amin <150836543+ruhulamin-fsd@users.noreply.github.com>
2025-01-26 11:08:46 +0600

    Update fileUploader.ts

--- src/helpers/fileUploader.ts
+++ src/helpers/fileUploader.ts
@@ -32,3 +32,42 @@ export const fileUploader = {
   updateProfileImage,
   uploadQuizImage,
 };
+
+
+// const storage = multer.memoryStorage();
+
+// const upload = multer({
+//   storage: storage,
+//   limits: { fileSize: 3000 * 1024 * 1024 }, // 3000 MB limit
+//   fileFilter: (req, file, cb) => {
+//     const allowedMimeTypes = [
+//       "image/jpeg",
+//       "image/png",
+//       "video/mp4",
+//       "video/x-matroska",
+//     ];
+//     if (!allowedMimeTypes.includes(file.mimetype)) {
+//       return cb(new Error("File type not allowed") as unknown as null, false);
+//     }
+//     cb(null, true);
+//   },
+// });
+
+// // upload single image
+// const courseImage = upload.single("courseImage");
+// const profileImage = upload.single("profileImage");
+// const coverPhoto = upload.single("coverPhoto");
+
+// // upload multiple image
+// const uploadMultiple = upload.fields([
+//   { name: "thumbnail", maxCount: 1 },
+//   { name: "classVideo", maxCount: 1 },
+// ]);
+
+// export const fileUploader = {
+//   upload,
+//   courseImage,
+//   uploadMultiple,
+//   profileImage,
+//   coverPhoto,
+// };
